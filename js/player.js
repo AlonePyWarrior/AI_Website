74cb8f8860491c0976e14078f910ac93895fb046
AlonePyWarrior <ali.esmaeilzadeh.py@gmail.com>
2023-11-09 14:53:01 +0330

    Add alert function to hold the maze at d-points

--- js/player.js
+++ js/player.js
@@ -8,6 +8,12 @@ class Player {
             x: this.position.x + this.width / 2,
             y: this.position.y + this.height / 2
         }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        this.speed = 0.3
     }
 
     draw() {
@@ -21,20 +27,55 @@ class Player {
         const waypoint = waypoints[this.waypointIndex]
         const yDistance = waypoint.y - this.center.y
         const xDistance = waypoint.x - this.center.x
+
         const angle = Math.atan2(yDistance, xDistance)
-        this.position.x += Math.cos(angle)
-        this.position.y += Math.sin(angle)
+
+        this.velocity.x = Math.cos(angle)
+        this.velocity.y = Math.sin(angle)
+
+        this.position.x += this.velocity.x * this.speed
+        this.position.y += this.velocity.y * this.speed
+
         this.center = {
             x: this.position.x + this.width / 2,
             y: this.position.y + this.height / 2
         }
 
         if (
-            Math.round(this.center.x) === Math.round(waypoint.x) &&
+            Math.abs(Math.round(this.center.x) - Math.round(waypoint.x)) < 
+            Math.abs(this.velocity.x * this.speed) &&
             Math.round(this.center.y) === Math.round(waypoint.y) &&
-            this.waypointIndex < waypoints.length - 1
+            // this.waypointIndex < waypoints.length - 1
+            this.waypointIndex < 26
         ) {
+            
+            // Implement a conditional statement here 
+            // that only if the user clicks on the button with the id "move_the_yellow"
+            // the index will increment
+
+            // Increment the index which means moving to the next cell on the maze
             this.waypointIndex++
+
+            // The Start Point
+            if (this.waypointIndex === 2) {
+                console.log("Start Point");
+                alert("Start Point\nLet's Go!")
+            }
+            
+            // All the decision points
+            let numbers = [7, 11, 14, 16]
+
+            // Check for decision points
+            if (numbers.includes(this.waypointIndex)) {
+                console.log("Decision point");
+                alert("Decision point!!!\nClick OK to continue")
+            }
+
+            // The End Point
+            if (this.waypointIndex === 25) {
+                console.log("End Point");
+                alert("End Point\nDone")
+            }
         }
     }
 }
