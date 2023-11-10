03cd534c0f9fbd6302979878d11030a3d85ce91d
AlonePyWarrior <ali.esmaeilzadeh.py@gmail.com>
2023-11-10 18:27:07 +0330

    Clean The project

--- Picture2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Picture2" tilewidth="32" tileheight="32" tilecount="78" columns="6">
- <image source="assets/Picture2.png" width="220" height="440"/>
-</tileset>
--- tileset_n.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="tileset_n" tilewidth="64" tileheight="64" tilecount="198" columns="18">
- <image source="assets/cells_n.png" width="1206" height="737"/>
-</tileset>
